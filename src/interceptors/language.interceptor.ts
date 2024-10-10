49ae097d6c6843f5812a062d16550155fd8104ae
SweetRetry <zimin.zhang2000@gmail.com>
2024-10-10 18:07:28 +0800

    feat: 增加语言拦截器,修改prisma service

---
+++ src/interceptors/language.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LanguageInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+
+    // 获取 Accept-Language 头信息
+    const acceptLanguage = request['accept-language'] || 'en-US';
+
+    console.log(
+      '🚀 ~ LanguageInterceptor ~ intercept ~ acceptLanguage:',
+      acceptLanguage,
+    );
+
+    // 设置语言信息到 request 对象上，方便后续使用
+    request['language'] = acceptLanguage; // 如果没有设置 Accept-Language，则使用默认语言
+
+    return next.handle(); // 继续传递请求
+  }
+}
